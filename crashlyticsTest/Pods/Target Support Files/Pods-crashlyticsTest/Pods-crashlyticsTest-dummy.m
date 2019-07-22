b3c8c968fe35d3e8a81c62dceeb476bc8fe2be09
Felipe Vergara <felipe.svergara@gmail.com>
2019-07-22 23:13:55 +1000

    Crashlytics added

---
+++ crashlyticsTest/Pods/Target Support Files/Pods-crashlyticsTest/Pods-crashlyticsTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_crashlyticsTest : NSObject
+@end
+@implementation PodsDummy_Pods_crashlyticsTest
+@end
--- crashlyticsTest/crashlyticsTest/ViewController.m
+++ crashlyticsTest/crashlyticsTest/ViewController.m
@@ -16,7 +16,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (IBAction)crash:(id)sender {
+    [NSException raise:@"Invalid Format" format:@"test Error"];
 }
 
 
